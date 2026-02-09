bd189c842045328c71bc1d9d71ed1d6887364b74
GetsEmSquishy <75450936+GetsEmSquishy@users.noreply.github.com>
2026-02-08 21:55:18 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+teams: [
+  { name: "SOLOMONO", signupChannelId: "1071192840408940626", raidSize: 20 },
+  { name: "CRIT HAPPENS", signupChannelId: "1248666830810251354", raidSize: 20 },
+  { name: "EARLY BIRD SPECIAL", signupChannelId: "1216844831767396544", raidSize: 20 },
+  { name: "WEEKEND WARRIORS", signupChannelId: "1338703521138081902", raidSize: 20 }
+],
